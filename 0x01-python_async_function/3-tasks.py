15fd8ef0e67785cbf3095bc808365beddc0e3002
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-01 16:40:10 +0300

    Corrected Argument Spelling

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,6 +4,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_depth: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Returns a Task Object"""
-    return asyncio.create_task(wait_random(max_depth))
+    return asyncio.create_task(wait_random(max_delay))
