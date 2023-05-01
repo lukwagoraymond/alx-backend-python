9a98ee03f40355467bf3225ceb57edeba66b3bb4
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-01 16:35:21 +0300

    Included Function Documentation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-
+"""Python Async Task Object"""
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_depth: int) -> asyncio.Task:
+    """Returns a Task Object"""
     return asyncio.create_task(wait_random(max_depth))
