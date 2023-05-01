ee8880aecc2742ffb62d25e80307ee82a7632e16
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-01 16:30:46 +0300

    Included Function Documentation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Python and Async Coroutines"""
 import asyncio
 from typing import List
 
@@ -7,6 +7,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a list of all delayed async coroutines"""
     tasks = [wait_random(max_delay) for i in range(n)]
     delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
     return delayed_tasks
