900d8ca978d9b52f6632f53abea327daf0e5c1ef
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-01 15:13:49 +0300

    Solutions to Advanced Tasks

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
-""""""
+"""Augmenting the following code with the correct duck-typed
+annotations"""
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Duck Typing - Fist element of a sequence"""
+    if lst:
+        return lst[0]
+    else:
+        return None
