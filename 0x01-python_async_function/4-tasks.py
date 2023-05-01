bcc91c5cdab39b60a50bd60e7b16e41ab9e1fc2e
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-01 16:11:58 +0300

    Corrected PEP Styling Issues

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Python and Async Task await function workflow"""
 import asyncio
 from typing import List
 
@@ -9,6 +9,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a list of time delays based on max_delay
     parameter"""
-    tasks = [task_wait_random(max_delay) for i in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
     return delayed_tasks
