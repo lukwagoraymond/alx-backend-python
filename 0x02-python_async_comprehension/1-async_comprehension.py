9ef8c304088f8ef4e807e501e5e75c1e8aa94634
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-02 20:02:48 +0300

    Corrected variable Annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> List[float]:
     """Results a list of random generated numbers
     from the async_generator coroutine function"""
     task = [i async for i in async_generator()]
